6dfea20edc09a24b753a771ab0480084cf92f1b6
prad <praditsas@open.gr>
2022-02-19 16:19:48 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const sendkeys = require('sendkeys')
+
+const key = ' ';
+const base_interval = 1000 * 60 * 5; // minutes
+const getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+const loop = () => {
+
+    sendkeys(key).catch((e) => console.error(e)).finally(()=> {
+
+        const next = (base_interval + getRandomIntInclusive(0, 1000 * 60));
+
+        console.log(`sending char code ${key.charCodeAt(0)} in ${(next/1000)} seconds`);
+
+        setTimeout(loop, next);
+
+    });
+    
+}
+
+const next = (base_interval + getRandomIntInclusive(0, 1000 * 60));
+
+console.log(`sending char code ${key.charCodeAt(0)} in ${(next/1000)} seconds`);
+
+setTimeout(loop, next);   
